3be92105fced5e13f7279f4158d3c366d9ae5db8
Owen Rees <owenrees.dev@gmail.com>
2026-03-08 23:34:10 +0100

    update test imports

--- exercises/practice/hello-world/.meta/generateTests.js
+++ exercises/practice/hello-world/.meta/generateTests.js
@@ -2,10 +2,10 @@ import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import getValidCases from '../../../../test_generator/getCases.js';
 import { generate, toPascalCase } from '../../../../test_generator/testGenerator.js';
-import * as config from './testTemplate.js';
+import { slug, assertionFunctions, template } from './testTemplate.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const cases = getValidCases(config.slug);
-const outputPath = path.resolve(__dirname, '..', 'tests', `${toPascalCase(config.slug)}_test.res`);
+const cases = getValidCases(slug);
+const outputPath = path.resolve(__dirname, '..', 'tests', `${toPascalCase(slug)}_test.res`);
 
-generate(outputPath, config.slug, cases);
+generate(outputPath, slug, cases, assertionFunctions, template);
