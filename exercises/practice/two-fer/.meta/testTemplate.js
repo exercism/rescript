6b7a83e04961ea83c7f6e5ecb2db082423c87838
Owen Rees <owenrees.dev@gmail.com>
2026-03-08 23:34:29 +0100

    new test template for two-fer, with options

--- exercises/practice/two-fer/.meta/testTemplate.js
+++ exercises/practice/two-fer/.meta/testTemplate.js
@@ -5,5 +5,9 @@ export const slug = 'two-fer';
 export const assertionFunctions = [ stringEqual ]
 
 export const template = (c) => {
-  return `stringEqual(~message="${c.description}", twoFer(), "${c.expected}")`
+  if (c.input.name) {
+    return `stringEqual(~message="${c.description}", twoFer(Some("${c.input.name}")), "${c.expected}")`
+  } else {
+    return `stringEqual(~message="${c.description}", twoFer(None), "${c.expected}")`
+  } 
 }
