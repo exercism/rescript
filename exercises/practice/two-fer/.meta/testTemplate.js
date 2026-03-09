18539f1a78c9dfbc0b7d5a7f0a2725725b38f93e
Owen Rees <owenrees.dev@gmail.com>
2026-03-09 08:26:12 +0100

    get slug name from filepath

--- exercises/practice/two-fer/.meta/testTemplate.js
+++ exercises/practice/two-fer/.meta/testTemplate.js
@@ -1,6 +1,9 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import { stringEqual } from "../../../../test_generator/assertions.js";
 
-export const slug = 'two-fer';
+export const slug = path.basename(path.resolve(__dirname, '..'))
 
 export const assertionFunctions = [ stringEqual ]
 
