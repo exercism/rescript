dce853b2bc79631848558b83991892969c409eae
Owen Rees <owenrees.dev@gmail.com>
2026-03-15 11:52:55 +0100

    add floatEqual comparator

--- exercises/practice/space-age/.meta/testTemplate.js
+++ exercises/practice/space-age/.meta/testTemplate.js
@@ -1,19 +1,19 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { assertEqual } from "../../../../test_generator/assertions.js";
+import { floatEqual } from "../../../../test_generator/assertions.js";
 import { generateTests } from '../../../../test_generator/testGenerator.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const slug = path.basename(path.resolve(__dirname, '..'))
 
 // EDIT THIS WITH YOUR ASSERTIONS
-export const assertionFunctions = [ assertEqual ]
+export const assertionFunctions = [ floatEqual ]
 
 // EDIT THIS WITH YOUR TEST TEMPLATES
 export const template = (c) => {
   const { planet, seconds } = c.input
 
-  return `assertEqual(~message="${c.description}", age(${planet}, ${seconds}), ${c.expected})`
+  return `floatEqual(~message="${c.description}", age(${planet}, ${seconds}), ${c.expected})`
 }
 
 generateTests(__dirname, slug, assertionFunctions, template)
