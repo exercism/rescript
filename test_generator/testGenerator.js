46c3496eb121a542788a55c08ef591d3d0844222
Owen <owenrees.dev@gmail.com>
2026-03-16 11:25:20 +0100

    remove commented code

--- test_generator/testGenerator.js
+++ test_generator/testGenerator.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import getValidCases from './getCases.js';
+import { getValidCases } from './GetCases.res.js';
 
 export const generateTests = (dir, slug, assertionFunctions, template) => {
     const outputPath = path.resolve(dir, '..', 'tests', `${toPascalCase(slug)}_test.res`);
